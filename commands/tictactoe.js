f7d66cbbbc35525ff7ffea6004e80d8d31eef6ac
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-18 13:25:43 +0200

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -22,7 +22,7 @@
 	},
 		ttt: 'tictactoe',
 		tictactoe: function(arg, by, room){
-		this.reply(user + 'wants to play a Tic Tac Toe game! use -join to join the game.')
+		this.reply(user + 'wants to play a Tic Tac Toe game! use -join to join the game.');
 		let p1 = user;
 	}
 }
