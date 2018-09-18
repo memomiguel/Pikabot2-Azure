e71bd20a260962690998c6b5b18d6af517817820
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-18 13:27:45 +0200

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -1,25 +1,4 @@
-	function send (room, str) {
-	Bot.say(room, str);
-	}
-
-	function joinArray (arr, str1) {
-	if (!arr.length) return '';
-	var txt = "**" + arr[0] + "**";
-	if (arr.length > 1) {
-		for (var i = 1; i < arr.length - 1; i++) txt += ", **" + arr[i] + "**";
-		txt += " " + str1 + " " + "**" + arr[arr.length - 1] + "**";
-	}
-	return txt;
-	}
-
 	exports.commands = {
-	j: 'join',
-	"in": 'join',
-	join: function (arg, by, room, cmd, game) {
-		if (by == p1) return this.reply('You are already in the game!');
-		game.userJoin(by);
-		let p2 = by;
-	},
 		ttt: 'tictactoe',
 		tictactoe: function(arg, by, room){
 		this.reply(user + 'wants to play a Tic Tac Toe game! use -join to join the game.');
