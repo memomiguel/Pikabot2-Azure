5e23dc7d9ab4c12bde11efa936d4f6c6b005cd79
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-18 13:32:03 +0200

    Update tictactoe.js

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -1,5 +1,5 @@
 	exports.commands = {
-		tictactoe: function(arg, by, room){
+		tictactoe: function(arg, user, room){
 		this.reply(user + 'wants to play a Tic Tac Toe game! use -join to join the game.');
 		let p1 = user;
 	}
