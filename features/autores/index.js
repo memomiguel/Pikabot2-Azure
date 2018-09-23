45d67f222e7ad1d1b2ee068522d0d03aa8892374
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-23 14:22:28 +0200

    Add files via upload

---
+++ features/autores/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.id = 'autoresp';
+exports.desc = '';
+
+exports.init = () => {};
+exports.parse = (room, message, isIntro, spl) => {
+	if (isIntro || room !== "izukuslair" || spl[0] !== "c:") return;
+	
+	let user = spl[2];
+	if (spl[3].includes("hello")) Bot.say(room, `Hey there ${toId(user)}!`);
+};
