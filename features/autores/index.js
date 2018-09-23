8f0ea9f94b61f2cce78576a93fb4d02fd5233228
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-23 19:00:26 +0200

    Update index.js

--- features/autores/index.js
+++ features/autores/index.js
@@ -8,5 +8,5 @@ exports.parse = (room, message, isIntro, spl) => {
 	if (isIntro || room !== "izukuslair" || spl[0] !== "c:") return;
 	
 	let user = spl[2];
-	if (spl[3].includes("hello" && "pikabot")) Bot.say(room, `Hey there ${toId(user)}!`);
+	if (spl[3].includes("hello" || "hi")) Bot.say(room, `Hey there ${toId(user)}!`);
 };
