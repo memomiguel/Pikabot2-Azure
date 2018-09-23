09abbf388cbefb582f9fa04a1ce6148ead248cb1
memomiguel <39495486+memomiguel@users.noreply.github.com>
2018-09-23 18:45:37 +0200

    Update index.js

--- features/autores/index.js
+++ features/autores/index.js
@@ -5,8 +5,12 @@ exports.desc = '';
 
 exports.init = () => {};
 exports.parse = (room, message, isIntro, spl) => {
-	if (isIntro || room !== "izukuslair" || spl[0] !== "c:") return;
+	if (isIntro) return;
 	
 	let user = spl[2];
-	if (spl[3].includes("hello")) Bot.say(room, `Hey there ${toId(user)}!`);
+	if (spl[3].includes(“thepikabot” || “ThePika-bot” || “pikabot” || “Pikabot”) || room == user) {
+           if (spl[3].includes("hello" || “hi”)) Bot.say(room, ‘Hey there ${toId(user)}!’);
+           if (spl[3].includes("when were you born")) Bot.say(room, ‘I was born on Friday, August 17, 2018. Thanks for asking, ${toId(user)}!’);
+           if (spl[3].includes("how are you")) Bot.say(room, ‘I am good! Thanks for asking, ${toId(user)}!’);
+         }
 };
