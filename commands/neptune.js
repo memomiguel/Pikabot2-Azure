ebf57fabb60ff9bc094be79ef8a91788590730d5
memomiguel <39495486+memomiguel@users.noreply.github.com>
2019-01-06 22:55:28 +0100

    Update neptune.js

--- commands/neptune.js
+++ commands/neptune.js
@@ -2,14 +2,14 @@
 
 exports.commands = {
   setseason: function (arg, user, room) {
-    if (room !== "neptune" || "pikachuvalley") return this.reply("something wrong here");
+    if (room !== "pikachuvalley") return this.reply("something wrong here");
     if (!this.can('pikareg')) return this.reply("``&`` required to use this command!");
     if (!arg) return this.reply("Help: ``" + this.cmdToken + "setseason [message]``");
     Db('message').set(news, arg);
     this.reply("Season message was succesfully set to: " + arg);
     },
   season: function (arg, user, room){
-    if (room !== "neptune" || "pikachuvalley") return;
+    if (room !== "pikachuvalley") return;
     let display = Db('message').get(news);
     if (!display) return this.reply("Nothing set now, use ``-setseason [message]`` to set a message");
     this.reply(display);
