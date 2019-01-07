6b10da7caa903638af4049f5392be457806015fa
memomiguel <39495486+memomiguel@users.noreply.github.com>
2019-01-07 14:30:07 +0100

    Update neptune.js

--- commands/neptune.js
+++ commands/neptune.js
@@ -3,13 +3,13 @@ exports.commands = {
  	   if (room !== "pikachuvalley") return this.reply("something wrong here");
  	   if (!this.can('pikareg')) return this.reply("``&`` required to use this command!");
  	   if (!arg) return this.reply("Help: ``" + this.cmdToken + "setseason [message]``");
- 	   Db('message').set(news, arg);
+ 	   Db('message').set("news", arg);
  	   this.reply("Season message was succesfully set to: " + arg);
  	 },
  	 season: function (arg, user, room){
  	   if (room !== "pikachuvalley") return;
- 	   if(Db('message').get(news)==NULL) return this.reply("Nothing set now, use ``-setseason [message]`` to set a message");
- 	   let display = Db('pika').get(news);
+ 	   if(Db('message').get("news")==NULL) return this.reply("Nothing set now, use ``-setseason [message]`` to set a message");
+ 	   let display = Db('pika').get("news");
  	   this.reply(display);
     	},
 };
