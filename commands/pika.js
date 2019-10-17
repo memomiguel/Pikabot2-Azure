118d57d02070292a90fef7065da82cbd5661b35f
memomiguel <39495486+memomiguel@users.noreply.github.com>
2019-10-17 16:33:12 +0200

    Update pika.js

--- commands/pika.js
+++ commands/pika.js
@@ -485,7 +485,7 @@ exports.commands = {
 		if (packId == "p1"){ 
 			if (playerMoney < 30) return this.reply("You dont have enough money to buy this pack");
 			setMoney(player, -30);
-			prize = Math.floor((Math.random() * 100));
+			let prize = Math.floor((Math.random() * 100));
 			pokemon = "pichu";
 			if (prize < 41){ 
 				level = 10;
