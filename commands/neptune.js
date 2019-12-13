74ecef66ed857cf073795580b14ea7cc38917ebd
memomiguel <39495486+memomiguel@users.noreply.github.com>
2019-12-13 22:29:06 +0100

    Update neptune.js

--- commands/neptune.js
+++ commands/neptune.js
@@ -15,7 +15,7 @@ exports.commands = {
 	tourprize: function (arg, user, room) {
 	   if (room !== "pikachuvalley") return this.reply("Atlantis exclusive command");
  	   if (!this.isRoomRanked(room, '&')) return this.reply("``&`` required to use this command!");
- 	   if (!arg) return this.reply("Help: ``" + this.cmdToken + "tourprize [1st], [2nd]");
+ 	   if (!arg) return this.reply("Help: ``" + this.cmdToken + "tourprize [1st], [2nd]``");
 	   let params = arg.split(",");
 	   if (params.length > 2 || params.length < 2 ) return this.reply("Help: ``" + this.cmdToken + "tourprize [1st], [2nd]``");
 		let prize1 = toId(params[0]);
@@ -23,7 +23,6 @@ exports.commands = {
 		var prz1 = parseFloat(prize1).toFixed(0);
 		var prz2 = parseFloat(prize2).toFixed(0);
       if (prz1 == 'NaN' || prz2 == 'NaN') return this.reply('Error: The Argument must be a number!');
-      if (prz1 < 0 || prz2 < 0) return this.reply('Error: The Argument cant be a negative number!');
-      if (prz1 > 0 || prz2 > 0) this.reply("/tour prizemoney " + prz1 + ", " + prz2);
+      this.reply("/tour prizemoney " + prz1 + ", " + prz2);
 	},
 };
