599bb0999e63b5dd0a96a325b5c0a463d59cfa5f
memomiguel <39495486+memomiguel@users.noreply.github.com>
2019-12-13 22:36:43 +0100

    Update neptune.js

--- commands/neptune.js
+++ commands/neptune.js
@@ -14,15 +14,14 @@ exports.commands = {
     	},
 	tourprize: function (arg, user, room) {
 	   if (room !== "pikachuvalley") return this.reply("Atlantis exclusive command");
- 	   if (!this.isRoomRanked(room, '&')) return this.reply("``&`` required to use this command!");
- 	   if (!arg) return this.reply("Help: ``" + this.cmdToken + "tourprize [1st], [2nd]``");
+ 	   if (!this.isRoomRanked(room, '%')) return this.reply("``%`` required to use this command!");
+ 	   if (!arg) return this.reply("Help: ``" + this.cmdToken + "tourprize [user], [prize]``");
 	   let params = arg.split(",");
-	   if (params.length > 2 || params.length < 2 ) return this.reply("Help: ``" + this.cmdToken + "tourprize [1st], [2nd]``");
+	   if (params.length > 2 || params.length < 2 ) return this.reply("Help: ``" + this.cmdToken + "tourprize [user], [prize]``");
 		let prize1 = toId(params[0]);
 		let prize2 = toId(params[1]);
-		var prz1 = parseFloat(prize1).toFixed(0);
 		var prz2 = parseFloat(prize2).toFixed(0);
-      if (prz1 == 'NaN' || prz2 == 'NaN') return this.reply('Error: The Argument must be a number!');
-      this.reply("/tour prizemoney " + prz1 + ", " + prz2);
+      if (prz2 == 'NaN') return this.reply('Error: The prize must be a number!');
+      this.reply("/transfer " + prize1 + ", " + prz2);
 	},
 };
